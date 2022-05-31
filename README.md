322a4ccebbfff0a5f20b0c0352345a1f289c76a2
Ricma <ric.madse@gmail.com>
2022-05-30 19:11:48 -0600

    html and css framework finished

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # weatherApp
+
+This is a weather app, designed to be able to search for the current weather of a city, and a five day forecast.  it will show temperature data, wind, humidity and UV index data, and will list cities previously searched for recall.
---
+++ assets/script.js
@@ -0,0 +1,9 @@
+const cityEl = document.getElementById("city")
+const tempEl = document.getElementById("temp")
+const windEl = document.getElementById("wind")
+const humidityEl = document.getElementById("humidity")
+const uVindexEl = document.getElementById("index-uv")
+// const cityEl = document.getElementById("city")
+// const cityEl = document.getElementById("city")
+
+
