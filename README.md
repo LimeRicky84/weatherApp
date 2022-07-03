56e3357fdcb3266489d7bcc46de4171b28781c21
Ricma <ric.madse@gmail.com>
2022-07-03 14:29:31 -0600

    javascript update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # weatherApp
 
 This is a weather app, designed to be able to search for the current weather of a city, and a five day forecast.  it will show temperature data, wind, humidity and UV index data, and will list cities previously searched for recall.
+
+deployed @ https://limericky84.github.io/weatherApp/
--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,84 @@ const tempEl = document.getElementById("temp")
 const windEl = document.getElementById("wind")
 const humidityEl = document.getElementById("humidity")
 const uVindexEl = document.getElementById("index-uv")
-// const cityEl = document.getElementById("city")
-// const cityEl = document.getElementById("city")
+const buttonEl = document.getElementById("search")
+const citySearch = document.querySelector("#city-search")
+const cityListEl = document.querySelector("#city-list")
+const apiKey = "3da86ef1a041c1f049c863a14b970862"
+let cities = []
+
+buttonEl.addEventListener("click", function(event) {
+// searchFunc is going to take the input from the search bar and request coordinates from Geocoding APi
+    event.preventDefault()
+    console.log("clicked")
+    let cityName = citySearch.value.trim()
+    console.log(cityName)
+    cities.concat(cityName)
+    storeCity()
+    renderCity()
+// searchFunc needs to log the coordinate response from Geocoding API in local storage
+    getWeatherResponse()  
+  
+});
+// another function is going to list the inputted city in html <button> tags below the search form
 
 
+// another function is going to send coordinate data to the weather API to return weather info and input it into the html
+const init = () => {
+
+    const storedCities = JSON.parse(localStorage.getItem('cities'))
+    if (storedCities !== null) {
+        cities = storedCities
+    }
+}
+
+const storeCity = () => {
+   
+    localStorage.setItem('cities', JSON.stringify(cities))
+    console.log(localStorage.cities)
+}
+
+const renderCity = () => {
+    if(cities.length === 0) {
+        cityListEl.textContent = "cities will list here"
+        return;
+    }
+
+    for(let i = 0; i < cities.length; i++) {
+        let city = cities[i]
+
+        let cityItemEl = document.createElement('button');
+        cityItemEl.classList = "btn btn-default"
+
+
+        cityListEl.appendChild(cityItemEl)
+
+    }
+    
+}
+
+// function to request weather data for stored coordinates for cities the page has already made a button for
+
+const getWeatherResponse = () => {
+    const queryApi = 'https://api.openweathermap.org/data/2.5/weather?q=' + citySearch.value + '&appid=' + apiKey;
+    console.log(citySearch.value)
+    console.log(apiKey)
+    fetch(queryApi)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response)
+                response.json().then(function (data) {
+                console.log(data)
+                // displayfunction...
+            })
+            } else {
+                alert('Error: ' + response.statusText)
+            }
+        })
+        .catch(function (error) {
+            alert("You're not even wrong bro!")
+        })
+
+}
+
+init()
